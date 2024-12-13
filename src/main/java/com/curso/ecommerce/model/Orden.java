46a635fe02d52c4244fd0bcb5002047dba839b10
WNeira05 <walterneira1986@gmail.com>
2024-12-13 16:13:09 -0300

    primer commit

---
+++ src/main/java/com/curso/ecommerce/model/Orden.java
@@ -0,0 +1,74 @@
+package com.curso.ecommerce.model;
+
+import java.time.LocalDate;
+
+public class Orden {
+
+    private Long id;
+    private String numero;
+    private LocalDate fechaCreacion;
+    private LocalDate fechaRecibida;
+    private double total;
+
+    public Orden() {
+    }
+
+    public Orden(Long id, String numero, LocalDate fechaCreacion, LocalDate fechaRecibida, double total) {
+        this.id = id;
+        this.numero = numero;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaRecibida = fechaRecibida;
+        this.total = total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public LocalDate getFechaRecibida() {
+        return fechaRecibida;
+    }
+
+    public void setFechaRecibida(LocalDate fechaRecibida) {
+        this.fechaRecibida = fechaRecibida;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Orden{" +
+                "id=" + id +
+                ", numero='" + numero + '\'' +
+                ", fechaCreacion=" + fechaCreacion +
+                ", fechaRecibida=" + fechaRecibida +
+                ", total=" + total +
+                '}';
+    }
+}
